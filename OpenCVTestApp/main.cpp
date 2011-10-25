41bcefaa2538cd6b78ce5325edf13d3e9f5d8055
Sachi <sachi@tilo-mitras-macbook-pro.local>
2011-10-24 22:22:03 -0400

    Initial Commit

---
+++ OpenCVTestApp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenCVTestApp
+//
+//  Created by Sachi on 11-10-24.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
